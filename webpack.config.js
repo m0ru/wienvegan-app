0f6afb7b250b853dec7ecaf7c8344fc90554ed1e
moru <e1028092@student.tuwien.ac.at>
2015-03-29 22:57:22 +0200

    Got example from esnunes/riotjs-loader to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   entry: "./scripts/wv.js",
   output: {
         path: __dirname + "/bin/",
@@ -12,4 +12,30 @@ module.exports = {
   node: {
     global: true
   }
-}
+}*/
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      riot: 'riot'
+    })
+  ],
+  module: {
+    preLoaders: [
+      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
+    ],
+    loaders: [
+      { test: /\.css$/, loader: "style!css"}
+    ]
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
